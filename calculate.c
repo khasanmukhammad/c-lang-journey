bda4de69df46689b0633e9f96debdbd5bb319e5a
Khasan <muhammadovich.09@gmail.com>
2025-05-26 12:10:20 +0500

    calculate

---
+++ calculate.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+int sumNums(int num1, int num2) {
+    return num1 + num2;
+}
+
+
+int main() {
+  
+   int num1, num2;
+
+   printf("Enter first value: ");
+   scanf("%d", &num1);
+
+   printf("Enter second value: ");
+   scanf("%d", &num2);
+
+   int result = sumNums(num1, num2);
+
+   printf("Result: %d\n", result);
+
+
+
+
+
+}
