7f983955a9946950dc9d2d508dc7846b62c8ea68
Khasan <muhammadovich.09@gmail.com>
2025-05-27 10:19:09 +0500

    quizloop

--- while,user_input.c
+++ while,user_input.c
@@ -6,12 +6,22 @@ int main() {
 
     while (1) {
         printf("37 + 40: ");
-        scanf("%d", &enteredNum);
 
-        if (enteredNum == answerNum) {
+        if (scanf("%d", &enteredNum) != 1) {
+            printf("Please entere number. Try again!\n");
+
+            int ch;
+            while ((ch = getchar()) != '\n' && ch != EOF);
+
+            continue; 
+        }
+
+        typeof(enteredNum) userInput = enteredNum;
+
+        if (userInput == answerNum) {
             printf("Congrats, your answer is correct. Answer number is %d\n", answerNum);
             break;
-        } else if (enteredNum < answerNum) {
+        } else if (userInput < answerNum) {
             printf("Too low. Try again.\n");
         } else {
             printf("Too high. Try again.\n");
