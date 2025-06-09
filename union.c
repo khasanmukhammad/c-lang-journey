83f5bf8c681013ec0d2dd944aaa783c4a5a1275a
Khasan <muhammadovich.09@gmail.com>
2025-06-09 15:40:00 +0500

    union

---
+++ union.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+union myUnion {
+    int myNum;
+    char myLetter;
+    char myString[30];
+};
+
+int main () {
+    union myUnion u1;
+    u1.myNum = 1000;
+    u1.myLetter = 'X';
+
+    printf("mynum %d\n", u1.myNum);
+    printf("myletter %c\n", u1.myLetter);
+    
+    return 0;
+}
