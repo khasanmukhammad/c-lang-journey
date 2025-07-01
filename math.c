347748a07a952b8c37ae525722ddd97fa0dd15de
Khasan <muhammadovich.09@gmail.com>
2025-07-01 16:19:53 +0500

    math

--- math.c
+++ math.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include <math.h>
 int main(){
-    float a, b, S, M, a_square, b_square;
+    float a, b, S, M, a_square, b_square, module;
 
     printf("Enter first number (a): ");
     scanf("%f", &a);
@@ -11,11 +12,15 @@ int main(){
     M = a * b;
     a_square = a * a;
     b_square = b * b;
-
+    module = fabs(a);
+    module = fabs(b);
+    
     printf("Sum of numbers: %f\n", S);
     printf("Multipication if numbers: %f\n", M);
     printf("a square: %f\n", a_square);
     printf("b square: %f\n", b_square);
-
+    printf("Module of numbers (a): %f\n", module);
+    printf("Module of numbers (b): %f\n", module);
+   
     return 0;
 }
