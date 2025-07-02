25698562226bfaca96b6e452d789fa8e94c0483c
Khasan <muhammadovich.09@gmail.com>
2025-07-02 10:44:12 +0500

    triangle

--- circle_diameter.c
+++ circle_diameter.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #define PI 3.14
 
-// Kvadrat ildiz olish funksiyasi
 float sqrtSimple(float n) {
     float i = 0;
-    float step = 0.00001; // aniqlik darajasi
+    float step = 0.00001;
 
     while (i * i <= n) {
         i += step;
@@ -16,15 +15,12 @@ float sqrtSimple(float n) {
 int main() {
     float S, d, R;
 
-    // Doira yuzasini kiritish
     printf("Enter the area of the circle: ");
     scanf("%f", &S);
 
-    // Radius va diametr hisoblash (sqrt o'rniga sqrtSimple ishlatilmoqda)
     R = sqrtSimple(S / PI);
     d = 2 * R;
 
-    // Natija chiqarish
     printf("Radius: %.2f\n", R);
     printf("Diameter: %.2f\n", d);
 
--- triangle.c
+++ triangle.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
 #include <math.h>
 
+double sqrtSimple(double n) {
+    double x = n;
+    double step = 0.00001; // aniqlik
+    double i = 0;
+
+    while (i * i <= n) {
+        i += step;
+    }
+
+    return i;
+}
+
+
 int main() {
     double a, b, c, perimetr;
 
     printf("2 ta katetni kiriting (a va b): ");
     scanf("%lf %lf", &a, &b);
 
-    c = sqrt(a * a + b * b);
+    c = sqrtSimple(a * a + b * b);
     perimetr = a + b + c;
 
     printf("Gipotenuza: %.2lf\n", c);
