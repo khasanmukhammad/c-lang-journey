fb8449003e9d56ffbbe6d81e09068d11524436dc
Khasan <muhammadovich.09@gmail.com>
2025-07-08 17:07:29 +0500

    days

---
+++ days.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int years, months, weeks,  days;
+    
+    printf("Enter days: " );
+    scanf("%d", &days);
+
+    years = days / 365;
+    
+    months = days /30;
+
+    weeks = days /7;
+
+    days = days % 7;
+
+    printf("Years: %d\n", years);
+    printf("Months: %d\n", months);
+    printf("Weeks: %d\n", weeks);
+    printf("Remain days: %d\n", days);
+
+    return 0;
+    
+}
