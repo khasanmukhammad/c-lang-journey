6c79f174ae78b7d1f25c5ac5cb667a2e56e104fc
Khasan <muhammadovich.09@gmail.com>
2025-09-04 17:02:50 +0500

    check intenger

---
+++ enum+week.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main () {
+    enum week{mon, tue, wed, thu, fri, sat, sun};
+
+    printf("Mon = %d\n", mon);
+    printf("Tue = %d\n", tue);
+    printf("Wed = %d\n", wed);
+    printf("Thu = %d\n", thu);
+    printf("Fri = %d\n", fri);
+    printf("Sat = %d\n", sat);
+    printf("Sun = %d\n", sun);
+
+    return 0;
+}
